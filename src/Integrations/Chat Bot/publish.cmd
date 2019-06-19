f88c696751885041258ccf594ec603edcbc4512c
Smith Codio <smithc@microsoft.com>
2019-06-19 11:08:27 -0700

    Clean up master repro for public preview
    Removed solutions and all other files from the master repo since we only want to share them at GA

--- src/Integrations/Chat Bot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=eh-msft-accelerator-helpdeskbot-Web-Deploy.pubxml -p:Password=9WgevWcj2XdYu4vyYg7uta9vdkWGraFrAPqW8epjCRyy8pQTi6crnPYlmoqY
-
